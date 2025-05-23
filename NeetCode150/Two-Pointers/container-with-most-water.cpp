f8e22fb41a8b7f19ac2184843497573e924a1e51
Anushika Gupta <83057189+Anushika-Gupta@users.noreply.github.com>
2025-05-24 01:33:26 +0530

    container-with-most-water.cpp

---
+++ NeetCode150/Two-Pointers/container-with-most-water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int l = 0, r = n-1;
+        int res = 0;
+        while(l < r){
+            int area = min(height[l],height[r]) * (r - l);
+            res = max(res,area);
+            if(height[l] < height[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return res;
+    }
+};
+//Time Complexity: O(n)
+//Space Complexity: O(1)
